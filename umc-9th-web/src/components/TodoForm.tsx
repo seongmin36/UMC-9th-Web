3b341494dfae7e31e462c7a6e814dbeac65a1c47
seongmin36 <whtjdals3625@gmail.com>
2025-09-15 01:10:33 +0900

    [Week01] 크롱/조성민 미션 제출

--- umc-9th-web/src/components/TodoForm.tsx
+++ umc-9th-web/src/components/TodoForm.tsx
@@ -1,37 +1,43 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState } from "react";
 
 interface TodoProps {
   add: (text: string) => void;
 }
 
 const TodoForm = ({ add }: TodoProps) => {
-  const [todo, setTodo] = useState<string>('');
+  const [todo, setTodo] = useState<string>("");
 
-  const onChange = useCallback((e) => {
+  // e : 강제 any타입 추론으로 인한 eslint 오류 (React.ChangeEvent<HTMLInpuElement>)
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(e.target.value);
   }, []);
 
+  //
   const addTodo = (e) => {
     e.preventDefault();
-    if (todo.trim() !== '') {
+    if (todo.trim() !== "") {
       add(todo);
-      setTodo('');
+      setTodo("");
     }
   };
+  console.log(todo);
 
   return (
     <>
-      <form className="flex justify-center items-center gap-2 mb-4 w-full">
+      <form
+        className="flex justify-center items-center gap-2 mb-4 w-full"
+        onSubmit={addTodo}
+      >
         <input
           type="text"
+          value={todo}
           placeholder="할 일 입력"
-          className="border border-gray-300 rounded-md min-w-80 px-2 py-1"
+          className="border border-gray-300 rounded-md min-w-80 px-2 py-1 "
           onChange={onChange}
         />
         <button
           type="submit"
-          className="text-white px-2 py-1 bg-[#17b75e] border rounded-lg cursor-pointer"
-          onClick={addTodo}
+          className="text-white px-2 py-1 bg-[#17b75e] hover:bg-[#0a9649] transition-colors border rounded-lg cursor-pointer"
         >
           할 일 추가
         </button>
--- umc-9th-web/src/components/TodoItem.tsx
+++ umc-9th-web/src/components/TodoItem.tsx
@@ -1,28 +1,26 @@
-import type { Todo } from '../types/Todo';
+import type { Todo } from "../types/Todo";
 
 interface TodoItemProps {
   todo: Todo;
+  onComplete: (id: number) => void;
   onDelete: (id: number) => void;
 }
 
-const TodoItem = ({ todo, onDelete }: TodoItemProps) => {
+const TodoItem = ({ todo, onComplete, onDelete }: TodoItemProps) => {
   return (
-    <section>
-      <li className="flex border-none shadow-sm gap-20 justify-between px-2 py-1 items-center bg-gray-100 rounded-md">
-        <p className="text-start">{todo.text}</p>
-        <button className="text-white font-normal px-2 py-1 bg-[#17b75e] border rounded-lg cursor-pointer">
-          <li className="flex border-none shadow-sm gap-20 justify-between px-2 py-1 items-center bg-gray-100 rounded-md">
-            <p>{todo.text}</p>
-            <button
-              onClick={() => onDelete(todo.id)}
-              className="text-white font-normal px-2 py-1 border rounded-lg bg-[#c4302b] cursor-pointer"
-            >
-              {todo.isDone ? '삭제' : '완료'}
-            </button>
-          </li>
-        </button>
-      </li>
-    </section>
+    <li className="flex border-none shadow-sm gap-20 justify-between px-2 py-1 items-center bg-gray-100 rounded-md mb-2">
+      <p className="text-start">{todo.text}</p>
+      <button
+        onClick={() => (todo.isDone ? onDelete(todo.id) : onComplete(todo.id))}
+        className={`text-white font-normal px-2 py-1 border rounded-lg cursor-pointer transition-colors ${
+          todo.isDone
+            ? "bg-[#c4302b] hover:bg-[#a82723]"
+            : "bg-[#17b75e] hover:bg-[#0a9649]"
+        }`}
+      >
+        {todo.isDone ? "삭제" : "완료"}
+      </button>
+    </li>
   );
 };
 
